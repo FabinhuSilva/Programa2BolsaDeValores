2fc847e360ad824b6d0e514b2d3246ba4ad41e37
FabinhuSilva <fabiano.fesilva@gmail.com>
2022-05-02 01:30:34 -0300

    progrmaa de calculo da bolsa de valores

---
+++ src/programa2bolsadevalores/Programa2BolsaDeValores.java
@@ -0,0 +1,69 @@
+
+package programa2bolsadevalores;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Fabino F Silva
+ */
+public class Programa2BolsaDeValores {
+
+    
+    public static void main(String[] args) {
+
+        /*
+        2. Faça um programa que receba o tipo de ação, ou seja, uma letra 
+        qualquer a ser comercializada na bolsa de valores, o preço de compra
+        e o preço de venda de cada ação e que calcule e mostre:
+ a quantidade de ações com lucro superior a R$ 1.000,00;
+ a quantidade de ações com lucro inferior a R$ 200,00.
+        Finalize com o tipo de ação ‘F’.
+        */
+        
+        /*
+Obtenha um caractere a partir da entrada utilizando Scanner.next().charAt(0) em Java
+Obter um char a partir da entrada utilizando System.in.read() em Java
+Obter um gráfico a partir da entrada utilizando InputStreamReader() em Java
+        */
+         
+        int acaolucromil,acaolucromenor200;
+        double valorCompraAcao, valorVendaAcao;
+        char nomeacao;
+        
+        
+        System.out.println("===> Bolsa de Valores <===");
+        System.out.println("Baseado na Letra de sua AÇÃO, defina o valor \n"
+                         + "de venda de Compra e Venda da mesma!\n");
+        
+
+         
+         do{
+         Scanner valorinicial = new Scanner(System.in);
+           
+             System.out.println("Digite sua a Letra da sua Ação: ");
+             nomeacao = valorinicial.next().charAt(0);
+             
+             System.out.println("\nDigite o Valor de compra desta Ação: ");
+             valorCompraAcao = valorinicial.nextDouble();
+             
+             System.out.println("\nDigite sua a Letra da sua Ação: ");
+             valorVendaAcao = valorinicial.nextDouble();
+             
+             System.out.println("Ação : "+ nomeacao+" valor de Compra: R$ "+valorCompraAcao+"  Valor"
+             + "de Venda: R$ "+valorVendaAcao);
+             
+                if((valorCompraAcao - valorVendaAcao)> 1000){
+                     acaolucromil++;
+                    }else if((valorCompraAcao - valorVendaAcao)<200){
+                    acaolucromenor200++;
+                 }             
+         }while(nomeacao!='f');
+         
+         System.out.println("Total de Ações\n Valor Lucro Acima de R$ 1 mil reais: "+acaolucromil+"\n"
+                 + "Valor Lucro Abaixo de 200 reais : "+acaolucromenor200);
+         
+         
+    }
+    
+}
